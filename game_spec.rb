e87ff2faec6cfc111ee78500477a32c69e473436
Sam Marten <sammarten@gmail.com>
2014-11-22 12:33:35 -0600

    Unit Testing Chapter

---
+++ game_spec.rb
@@ -0,0 +1,8 @@
+require_relative 'game'
+
+describe Game do
+  
+  it "" do
+    
+  end
+end
---
+++ player_spec.rb
@@ -0,0 +1,50 @@
+require_relative 'player'
+
+describe Player do
+  before do
+    $stdout = StringIO.new
+    @initial_health = 150
+    @player = Player.new("larry", @initial_health)
+  end
+  
+  it "has a capitalized name" do
+    expect(@player.name).to eq("Larry")
+  end
+  
+  it "has an initial health" do
+    expect(@player.health).to eq(@initial_health)
+  end
+  
+  it "has a string representation" do
+    expect(@player.to_s).to eq("I'm Larry with a health of 150 and a score of 155.")
+  end
+  
+  it "computes a score as the sum of its health and length of name" do
+    expect(@player.score).to eq(@initial_health + @player.name.length)    
+  end
+  
+  it "increases health by 15 when w00ted" do
+    @player.w00t
+    
+    expect(@player.health).to eq(@initial_health + 15)
+  end
+  
+  it "decreases health by 10 when blammed" do
+    @player.blam
+    
+    expect(@player.health).to eq(@initial_health - 10)
+  end
+  
+  context "created with a default health" do
+    before do
+      @player = Player.new("larry")
+    end
+    
+    it "has a health of 100" do
+      expect(@player.health).to eq(100)
+    end
+  end
+end
+
+# rspec player_spec.rb --color
+# rspec player_spec.rb --color --format doc
